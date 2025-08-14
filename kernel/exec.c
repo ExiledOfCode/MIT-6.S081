a2efd672a1ce8a930ff0c68264ffcfebd95b8a99
jiyuan <wangjiyuan@jiyuandeMacBook-Air.local>
2025-08-14 20:02:36 +0800

    usyscall tablepage print ok

--- kernel/exec.c
+++ kernel/exec.c
@@ -8,6 +8,7 @@
 #include "elf.h"
 
 static int loadseg(pde_t *, uint64, struct inode *, uint, uint);
+void vmprint(pagetable_t pagetable, uint64 depth);
 
 int flags2perm(int flags)
 {
@@ -119,7 +120,7 @@ exec(char *path, char **argv)
     if(*s == '/')
       last = s+1;
   safestrcpy(p->name, last, sizeof(p->name));
-    
+
   // Commit to the user image.
   oldpagetable = p->pagetable;
   p->pagetable = pagetable;
@@ -127,6 +128,9 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
+  if (p->pid == 1){
+    vmprint(p->pagetable,0);
+  }
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
@@ -161,6 +165,6 @@ loadseg(pagetable_t pagetable, uint64 va, struct inode *ip, uint offset, uint sz
     if(readi(ip, 0, (uint64)pa, offset+i, n) != n)
       return -1;
   }
-  
+
   return 0;
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -45,7 +45,7 @@ kvmmake(void)
 
   // allocate and map a kernel stack for each process.
   proc_mapstacks(kpgtbl);
-  
+
   return kpgtbl;
 }
 
@@ -154,7 +154,7 @@ mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm)
 
   if(size == 0)
     panic("mappages: size");
-  
+
   a = va;
   last = va + size - PGSIZE;
   for(;;){
@@ -345,7 +345,7 @@ void
 uvmclear(pagetable_t pagetable, uint64 va)
 {
   pte_t *pte;
-  
+
   pte = walk(pagetable, va, 0);
   if(pte == 0)
     panic("uvmclear");
@@ -448,4 +448,21 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+void vmprint(pagetable_t pagetable, int depth) {
+    // 遍历所有512个PTE
+    if(depth > 2)
+      return ;
+    if(depth == 0)
+      printf("page table %p\n", pagetable);
+    for(int i = 0; i < 512; i++) {
+        pte_t pte = pagetable[i];
+        if(pte & PTE_V) {
+          for(int i = 0;i <= depth ;i++)
+            printf(" ..");
+          printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+          vmprint((pagetable_t)PTE2PA(pte), depth + 1);
+        }
+    }
 }
